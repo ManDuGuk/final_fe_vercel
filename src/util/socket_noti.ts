997e33a04f6ab0d80eb35b76be38e1bda7f81513
Yunho Nam <101496294+ManDuGuk@users.noreply.github.com>
2025-01-13 17:23:08 +0900

    socket이 시작되는 코드 수정

--- src/util/socket_noti.ts
+++ src/util/socket_noti.ts
@@ -1,6 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000"); // WebSocket 서버 URL 확인
+// const socket = io("http://localhost:4000"); // WebSocket 서버 URL 확인
+const socket = io("http://whatcpu.p-e.kr", { //서버쪽과 연결
+  transports: ["websocket"],
+  withCredentials: true,
+  reconnection: true, // 재연결 허용
+  reconnectionAttempts: 5, // 최대 시도 횟수
+  reconnectionDelay: 2000 // 재연결 간 간격(ms)
+});
+
 
 socket.on("new_notification", (data) => {
   console.log("New notification received:", data);
